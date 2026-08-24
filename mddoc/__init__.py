325ab5e196626833e1483df45bc683e473f0e5d3
iliass-wakkar <iliasswakkar.wip@gmail.com>
2026-08-24 19:15:49 +0100

    Fix Pyodide WebAssembly bundle imports for in-browser conversion

--- mddoc/__init__.py
+++ mddoc/__init__.py
@@ -5,8 +5,16 @@ __version__ = "1.0.0"
 
 from .themes import THEMES, get_theme
 from .native_converter import convert_markdown_to_docx
-from .pandoc_converter import convert_with_pandoc, is_pandoc_available
-from .templates import generate_reference_docx, generate_all_reference_templates
+
+try:
+    from .pandoc_converter import convert_with_pandoc, is_pandoc_available
+    from .templates import generate_reference_docx, generate_all_reference_templates
+except ImportError:
+    # Running in browser / WebAssembly environment without subprocess
+    convert_with_pandoc = None
+    is_pandoc_available = lambda: False
+    generate_reference_docx = None
+    generate_all_reference_templates = None
 
 __all__ = [
     "convert_markdown_to_docx",
